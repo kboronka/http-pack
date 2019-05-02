2da4b61ad913c11a8ebf984d6870814acf7cb757
Kevin Boronka <kboronka@gmail.com>
2019-05-01 21:10:29 -0400

    updated exit message

--- example/Program.cs
+++ example/Program.cs
@@ -5,18 +5,18 @@ using HttpPack.Server;
 
 namespace example
 {
-	class Program
-	{
-		public static void Main(string[] args)
-		{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
             TestTLS.TLS11();
             TestBasics.Basics();
 
             var publicFolder = Path.Combine(Directory.GetCurrentDirectory(), @"..\..\public");
             var server = new HttpServer(4600, publicFolder, null);
-            
-            Console.Write("Press any key to continue . . . ");
-			Console.ReadKey(true);
-		}
+
+            Console.Write("Press any key to stop http server . . . ");
+            Console.ReadKey(true);
+        }
     }
 }
