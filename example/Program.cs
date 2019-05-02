a6efc542f2178ee4011a40c3065e8b68bf8aeca5
Kevin Boronka <kboronka@gmail.com>
2019-05-01 20:32:19 -0400

    added http server example

--- example/Program.cs
+++ example/Program.cs
@@ -1,4 +1,7 @@
 ﻿using System;
+using System.IO;
+
+using HttpPack.Server;
 
 namespace example
 {
@@ -9,6 +12,9 @@ namespace example
             TestTLS.TLS11();
             TestBasics.Basics();
 
+            var publicFolder = Path.Combine(Directory.GetCurrentDirectory(), @"..\..\public");
+            var server = new HttpServer(4600, publicFolder, null);
+            
             Console.Write("Press any key to continue . . . ");
 			Console.ReadKey(true);
 		}
