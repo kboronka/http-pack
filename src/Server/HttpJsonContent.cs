f4fbd33b75e7758c78f0eae7f182662efd636b5f
Kevin Boronka <kboronka@gmail.com>
2019-05-01 20:54:29 -0400

    added HttpJsonContent

---
+++ src/Server/HttpJsonContent.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Text;
+
+using HttpPack.Json;
+using HttpPack.Fsm;
+
+namespace HttpPack.Server
+{
+    public class HttpJsonContent : HttpContent
+    {
+        public HttpJsonContent(JsonKeyValuePairs json) : base()
+        {
+            var body = json.Stringify();
+
+            this.content = Encoding.UTF8.GetBytes(body);
+            this.ContentType = "application/json";
+        }
+    }
+}
