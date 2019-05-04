15ef83d1b4deb7b01dace054bbd1161d4135a375
Kevin Boronka <kboronka@gmail.com>
2019-05-04 11:10:30 -0400

    added Url.Combine, inspired by Path.Combine

---
+++ src/Client/Url.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace HttpPack.Client
+{
+    public static class Url
+    {
+        public static string Combine(params string[] parts)
+        {
+            if (parts == null)
+            {
+                throw new ArgumentNullException(nameof(parts));
+            }
+
+            var result = "";
+            foreach (var part in parts)
+            {
+                if (string.IsNullOrEmpty(part))
+                {
+                    continue;
+                }
+
+                result = CombineUrlParts(result, part);
+            }
+
+            return EscapeUriString(result);
+        }
+
+        private static string CombineUrlParts(string a, string b)
+        {
+            char seperator = '/';
+
+            if (string.IsNullOrEmpty(a)) return b;
+            if (string.IsNullOrEmpty(b)) return a;
+            return a.TrimEnd(seperator) + seperator + b.TrimStart(seperator);
+        }
+
+        public static string EscapeUriString(string s)
+        {
+            if (string.IsNullOrEmpty(s))
+            {
+                return s;
+            }
+
+            s = s.Replace(" ", "+");
+            return Uri.EscapeUriString(s);
+        }
+    }
+}
