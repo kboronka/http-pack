81eb3dfcde8e870cd8fa6ce41f95358dbb789585
Kevin Boronka <kboronka@gmail.com>
2022-02-14 00:29:09 -0500

    cleanup

--- example/Program.cs
+++ example/Program.cs
@@ -2,30 +2,29 @@
 using System.IO;
 using HttpPack;
 
-namespace example
+namespace example;
+
+internal class Program
 {
-    internal class Program
+    public static void Main()
     {
-        public static void Main()
-        {
-            TestTLS.TLS11();
-            TestBasics.Basics();
-            Console.WriteLine();
+        TestTLS.TLS11();
+        TestBasics.Basics();
+        Console.WriteLine();
 
-            var publicFolder = Path.Combine(Directory.GetCurrentDirectory(), @"..\..\..\public");
-            var port = 4600;
-            var server = new HttpServer(port, publicFolder, null);
-            Console.WriteLine("Listening on port {0}", port);
+        var publicFolder = Path.Combine(Directory.GetCurrentDirectory(), @"..\..\..\public");
+        const int port = 4600;
+        var server = new HttpServer(port, publicFolder, null);
+        Console.WriteLine("Listening on port {0}", port);
 
-            var noFolderServer = new HttpServer(port + 1, null, null);
-            Console.WriteLine("Listening on port {0}", port + 1);
+        var noFolderServer = new HttpServer(port + 1, null, null);
+        Console.WriteLine("Listening on port {0}", port + 1);
 
 
-            Console.WriteLine();
-            Console.Write("Press any key to stop http server . . . ");
-            Console.ReadKey(true);
-            Console.WriteLine();
-            Console.Write("Shutting down . . . ");
-        }
+        Console.WriteLine();
+        Console.Write("Press any key to stop http server . . . ");
+        Console.ReadKey(true);
+        Console.WriteLine();
+        Console.Write("Shutting down . . . ");
     }
 }
--- example/TestPost.cs
+++ example/TestPost.cs
@@ -1,65 +1,65 @@
 ﻿using System;
 using HttpPack;
 
-namespace example
+namespace example;
+
+public static class TestBasics
 {
-    public static class TestBasics
+    public static bool Basics()
     {
-        public static bool Basics()
+        var postOne = GetExample(1);
+        Console.WriteLine("GET Example response: " + postOne.Stringify());
+        Console.WriteLine();
+
+        var newPost = new Post("foo", "bar", 1);
+        var postResponse = PostExample(newPost, "");
+        Console.WriteLine("POST Example response: " + postResponse);
+        Console.WriteLine();
+
+        return true;
+    }
+
+    /// <summary>
+    ///     HTTP GET example
+    /// </summary>
+    /// <param name="id"></param>
+    /// <returns></returns>
+    private static JsonKeyValuePairs GetExample(int id)
+    {
+        var uri = $"https://jsonplaceholder.typicode.com/posts/{id}";
+        var client = new HttpClient<JsonKeyValuePairs>();
+        var res = client.Get(uri, "");
+
+        if (res.Code == 200)
         {
-            var postOne = GetExample(1);
-            Console.WriteLine("GET Example response: " + postOne.Stringify());
-            Console.WriteLine();
-
-            var newPost = new Post("foo", "bar", 1);
-            var postResponse = PostExample(newPost, "");
-            Console.WriteLine("POST Example response: " + postResponse);
-            Console.WriteLine();
-
-            return true;
+            return res.Body;
         }
 
-        /// <summary>
-        ///     HTTP GET example
-        /// </summary>
-        /// <param name="id"></param>
-        /// <returns></returns>
-        private static JsonKeyValuePairs GetExample(int id)
+        return null;
+    }
+
+    /// <summary>
+    ///     HTTP POST example
+    /// </summary>
+    /// <param name="post"></param>
+    /// <param name="auth"></param>
+    /// <returns></returns>
+    private static int PostExample(Post post, string auth)
+    {
+        var req = new JsonKeyValuePairs
         {
-            var uri = string.Format("https://jsonplaceholder.typicode.com/posts/{0}", id);
-            var client = new HttpClient<JsonKeyValuePairs>();
-            var res = client.Get(uri, "");
+            {"body", post}
+        };
 
-            if (res.Code == 200)
-            {
-                return res.Body;
-            }
+        var uri = "https://jsonplaceholder.typicode.com/posts";
+        var client = new HttpClient<JsonKeyValuePairs>();
+        var res = client.Post(uri, req, auth);
 
-            return null;
+        if (res.Code == 201) // 201 = Created
+        {
+            return (int) res.Body["id"];
         }
 
-        /// <summary>
-        ///     HTTP POST example
-        /// </summary>
-        /// <param name="post"></param>
-        /// <returns></returns>
-        private static int PostExample(Post post, string auth)
-        {
-            var req = new JsonKeyValuePairs
-            {
-                {"body", post}
-            };
-
-            var uri = "https://jsonplaceholder.typicode.com/posts";
-            var client = new HttpClient<JsonKeyValuePairs>();
-            var res = client.Post(uri, req, auth);
-
-            if (res.Code == 201) // 201 = Created
-            {
-                return (int) res.Body["id"];
-            }
-
-            return -1;
-        }
+        return -1;
     }
 }
