75094d1b98d97db4fe7f8fd97fd18a1d828a30a0
Kevin Boronka <kboronka@gmail.com>
2022-02-24 23:09:37 -0500

    added a unit test

--- HttpPack.Tests/UnitTest1.cs
+++ HttpPack.Tests/UnitTest1.cs
@@ -1,11 +1,17 @@
+using HttpPack.Json;
 using Xunit;
 
 namespace HttpPack.Tests;
 
 public class UnitTest1
 {
-    [Fact]
-    public void Test1()
+    [Theory]
+    [InlineData("{\"test\":\"abc\"}", "test", "abc")]
+    public void ParseKeyValuePair(string json, string key, object value)
     {
+        var kvp = new JsonKeyValuePairs(json);
+        Assert.True(kvp.ContainsKey(key));
+        Assert.True(kvp.ValidateKey(key));
+        Assert.Equal(value, kvp[key]);
     }
 }
